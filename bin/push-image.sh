eee1ee530536c2bd3b86f82c23e8bc40aa07d004
Ben Jones <ben@bnjns.uk>
2020-02-16 20:32:49 +0000

    update registry url

--- bin/push-image.sh
+++ bin/push-image.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker tag bts/php:7.4 registry.gitlab.com/backstage-technical-services/infra/php:7.4
-docker push registry.gitlab.com/backstage-technical-services/infra/php:7.4
+docker tag bts/php:7.4 registry.gitlab.com/backstage-technical-services/website/php-docker:7.4
+docker push registry.gitlab.com/backstage-technical-services/website/php-docker:7.4
