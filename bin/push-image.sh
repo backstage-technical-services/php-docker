decbd8363b56190c188bc7c3f7014dd62575fdaa
Ben Jones <ben@bnjns.uk>
2020-06-12 16:41:27 +0100

    migrate to github

--- bin/push-image.sh
+++ bin/push-image.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker tag bts/php:7.4 registry.gitlab.com/backstage-technical-services/website/php-docker:7.4
-docker push registry.gitlab.com/backstage-technical-services/website/php-docker:7.4
+docker tag bts/php:7.4 docker.pkg.github.com/backstage-technical-services/php-docker/php:7.4
+docker push docker.pkg.github.com/backstage-technical-services/php-docker/php:7.4
